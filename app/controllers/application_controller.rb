0eed1c27c49802d3ef759477d08583fc738ffd16
kchothe <kchothe@paragyte.com>
2014-08-27 13:20:13 +0530

    Fixed Cross-Origin Resource Sharing (CORS)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,27 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
   
- #  after_filter  :set_csrf_cookie_for_ng
+  after_filter  :set_csrf_cookie_for_ng, :allow_cors
 
- #  def set_csrf_cookie_for_ng
- #    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
- #  end
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
- #  protected
+  protected
 
- #  # def verified_request?
- #  #   super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
- #  # end 
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end 
 
- #  def verified_request?
- #    if request.content_type == "application/json"
- #      true
- #    else
- #      super()
- #    end
- # end
+  def allow_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+
+    head(:ok) if request.request_method == "OPTIONS"
+    
+  end
+
+
 end
